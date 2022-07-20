de16bc116d7eda08cd652f377d79489f217bf7b3
OnlyMeHere <bennett45.j.m@gmail.com>
2022-07-19 18:22:11 -0700

    add index.js and test.js files

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// first set of questions ask for team manager's name, Emplyee ID, email, & office #.
+
+
+
+
+// next a menue with options to add and engineer or intern.
+
+
+
+
+// selecting engineer option prompt to enter name, ID, email, & GitHub username.
+
+// then return to the options menue.
+
+// selecting intern option prompts to enter name, ID, school.
+
+// then return to options menue.
+
+// when done then exit the application generates the HTML.
